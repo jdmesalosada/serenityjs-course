7ea3449f0af4c22af5e8f709c9365bf5d08d0525
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2019-01-12 10:44:07 -0500

    adjusted question

--- src/login/questions/get_invalid_login_message.ts
+++ src/login/questions/get_invalid_login_message.ts
@@ -3,9 +3,9 @@ import { LoginFail } from "../ui/login_fail";
 import { Wait, Is, Text } from "serenity-js/lib/serenity-protractor";
 
 
-export const ActualInvalidLoginMessage = () => Question.about('#actor get the invalid credentials message',
-    actor => {
-        Wait.until(LoginFail.Invalid_Credentials_Message, Is.visible()),
-            Text.of(LoginFail.Invalid_Credentials_Message)
-    }
+export const ActualInvalidLoginMessage = () => Question.about('#actor get the target value',
+    actor => Text.of(LoginFail.Invalid_Credentials_Message).answeredBy(actor) as PromiseLike<string>
 );
+
+
+
